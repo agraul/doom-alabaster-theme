a1e7accda9cc84604895a9d6723df355a1b8461a
Alexander Graul <agraul@suse.com>
2023-04-12 13:21:14 +0200

    Add screenshots

---
+++ alabaster_example.rs
@@ -0,0 +1,24 @@
+mod A;
+
+#[derive(Debug)]
+static B: i32 = 1;
+enum D { D1, D2 }
+struct E { e1: D, e2: D}
+union F { e1: D, e2: D}
+type G = E;
+
+trait H { fn h () -> (); }
+impl H for E { fn h () -> () { /* */ }; }
+
+fn d () -> D {
+  D{1,2}
+}
+
+fn i () -> () { }
+
+fn main() {
+  i();
+}
+
+"string"
+/* adas */
